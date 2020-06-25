72465c98dcc1f5d43670e74f9a02362f0b044b66
Felipe Vera <fvera@transapp.cl>
2020-06-25 18:54:06 -0400

    construccion del grafo extendido toma forma con la creacion de 3 tipos de nodos (de ciudad, de parada y de ruta). Se crea estructura auxiliar (tree_graph) el cual es un diccionario de nodos de modos de transporte y rutas para cada nodo de ciudad que es util para generar los nodos del grafo extendido. Se generan los metodo para crear todos los nodos del grafo extendido

--- sidermit/extended_graph.py
+++ sidermit/extended_graph.py
@@ -1,3 +1,9 @@
+from collections import defaultdict
+from enum import Enum
+
+from sidermit.publictransportsystem.network import RouteType
+
+
 class ExtendedNode:
     def __init__(self, extendend_node_id):
         self.id = extendend_node_id
@@ -9,81 +15,141 @@ class ExtendedNode:
 
 class CityNode(ExtendedNode):
 
-    def __init__(self, city_node_id, city_node):
+    def __init__(self, city_node_id, graph_node):
         ExtendedNode.__init__(self, city_node_id)
-        self.city_node = city_node
+        self.graph_node = graph_node
 
 
 class StopNode(ExtendedNode):
-    def __init__(self, stop_node_id, mode_name, city_node):
+    def __init__(self, stop_node_id, mode_obj, city_node):
         ExtendedNode.__init__(self, stop_node_id)
-        self.mode_name = mode_name
+        self.mode = mode_obj
         self.city_node = city_node
 
 
 class RouteNode(ExtendedNode):
-    def __init__(self, route_node_id, route_id, stop_node):
+    def __init__(self, route_node_id, route_obj, direction, stop_node):
         ExtendedNode.__init__(self, route_node_id)
-        self.route_id = route_id
+        self.route = route_obj
+        self.direction = direction
         self.stop_node = stop_node
 
 
 class ExtendedEdge:
-    def __init__(self, extended_edge_id, nodei, nodej, t, f):
+    def __init__(self, extended_edge_id, nodei, nodej, t, f, edge_type):
         self.id = extended_edge_id
         self.nodei = nodei
         self.nodej = nodej
         self.t = t
         self.f = f
+        self.type = edge_type
 
 
-class AccessEdge(ExtendedEdge):
-    def __init__(self, access_edge_id, nodei, nodej, t, f):
-        ExtendedEdge.__init__(self, access_edge_id, nodei, nodej, t, f)
-
-
-class BoardingEdge(ExtendedEdge):
-    def __init__(self, access_edge_id, nodei, nodej, t, f):
-        ExtendedEdge.__init__(self, access_edge_id, nodei, nodej, t, f)
-
-
-class AlightingEdge(ExtendedEdge):
-    def __init__(self, access_edge_id, nodei, nodej, t, f):
-        ExtendedEdge.__init__(self, access_edge_id, nodei, nodej, t, f)
-
-
-class RouteEdge(ExtendedEdge):
-    def __init__(self, access_edge_id, nodei, nodej, t, f):
-        ExtendedEdge.__init__(self, access_edge_id, nodei, nodej, t, f)
+class ExtendedEdgesType(Enum):
+    ACCESS = 1
+    BOARDING = 2
+    ALIGHTING = 3
+    ROUTE = 4
 
 
 class ExtendedGraph:
 
-    def __init__(self, graph_obj, mode_obj, network_obj):
+    def __init__(self, graph_obj, network_obj):
+
         self.__graph_obj = graph_obj
-        self.__mode_obj = mode_obj
         self.__network_obj = network_obj
 
+        # list with all city nodes
         self.__city_nodes = []
+        # to add city nodes
+        self.__add_city_nodes()
+
+        # assistant dictionary to build extended graph dic[node city][mode_obj] = list of routes
+        self.__tree_graph = defaultdict(lambda: defaultdict(list))
+        # build assistant dictionary
+        self.__build_tree_graph()
+
+        # list with all stop nodes, there are 1 stop node for each mode transiting in a city node
         self.__stop_nodes = []
+        # list with tuples (mode_obj, city_node) to not duplicated more of a stop for each mode in a city node
+        self.__mode_city = []
+        # to add stop nodes
+        self.__add_stop_nodes()
+
+        # list with all route nodes, there are 1 route node connected to a stop node for each route in a city node
         self.__route_nodes = []
+        self.__add_route_nodes()
 
         self.__access_edges = []
         self.__boarding_edges = []
         self.__alighting_edges = []
         self.__routes_edges = []
 
-        self.__add_city_nodes()
+    def get_tree_graph(self):
+        return self.__tree_graph
 
+    def get_city_nodes(self):
+        return self.__city_nodes
+
+    def get_stop_nodes(self):
+        return self.__stop_nodes
+
+    def get_route_nodes(self):
+        return self.__route_nodes
 
     def __add_city_nodes(self):
         for node in self.__graph_obj.get_nodes():
             city_node = CityNode(len(self.__city_nodes), node)
             self.__city_nodes.append(city_node)
 
-    def __add_stop_nodes(self):
+    def __build_tree_graph(self):
 
         for city_node in self.__city_nodes:
-            
+            node_graph_id = city_node.graph_node.id
 
+            routes = self.__network_obj.get_routes()
+            for route in routes:
+                mode_obj = route.mode
+                stops_i = route.stops_sequence_i
+                stops_r = route.stops_sequence_r
 
+                for stop in stops_i:
+                    if str(node_graph_id) == str(stop):
+                        # to avoid add twice first and last node in circular routes
+                        if route._type == RouteType.PREDEFINED:
+                            if (route, "I") not in self.__tree_graph[city_node][mode_obj]:
+                                self.__tree_graph[city_node][mode_obj].append((route, "I"))
+                        else:
+                            self.__tree_graph[city_node][mode_obj].append((route, "I"))
+
+                for stop in stops_r:
+                    if str(node_graph_id) == str(stop):
+                        # to avoid add twice first and last node in circular routes
+                        if route._type == RouteType.PREDEFINED:
+                            if (route, "R") not in self.__tree_graph[city_node][mode_obj]:
+                                self.__tree_graph[city_node][mode_obj].append((route, "R"))
+                        else:
+                            self.__tree_graph[city_node][mode_obj].append((route, "R"))
+
+    def __add_stop_nodes(self):
+
+        for city_node in self.__tree_graph:
+            for mode_obj in self.__tree_graph[city_node]:
+                if (mode_obj, city_node) not in self.__mode_city:
+                    self.__mode_city.append((mode_obj, city_node))
+                    stop_node = StopNode(len(self.__stop_nodes), mode_obj, city_node)
+                    self.__stop_nodes.append(stop_node)
+
+    def __add_route_nodes(self):
+
+        for city_node in self.__tree_graph:
+            for mode_obj in self.__tree_graph[city_node]:
+                stop_node = None
+                # to get stop node
+                for stop in self.__stop_nodes:
+                    if stop.city_node == city_node and stop.mode == mode_obj:
+                        stop_node = stop
+                        break
+                for route, direction in self.__tree_graph[city_node][mode_obj]:
+                    route_node = RouteNode(len(self.__route_nodes), route, direction, stop_node)
+                    self.__route_nodes.append(route_node)
---
+++ tests/test_extended_graph.py
@@ -0,0 +1,96 @@
+
+import unittest
+
+from sidermit.city import graph
+from sidermit.publictransportsystem import TransportNetwork, TransportModeManager, TransportMode
+from sidermit.extended_graph import ExtendedGraph
+
+
+class test_extended_graph(unittest.TestCase):
+
+    def test_build_tree_graph(self):
+        """
+        test methods get_tree_graph
+        :return:
+        """
+        graph_obj = graph.Graph.build_from_parameters(n=5, l=1000, g=0.5, p=2)
+        network = TransportNetwork(graph_obj)
+
+        mode_manager = TransportModeManager()
+        bus_obj = mode_manager.get_mode("bus")
+        metro_obj = mode_manager.get_mode("metro")
+        train_obj = TransportMode("train", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+
+        feeder_routes = network.get_feeder_routes(bus_obj)
+        radial_routes = network.get_radial_routes(metro_obj)
+        circular_routes = network.get_circular_routes(train_obj)
+
+        for route in feeder_routes:
+            network.add_route(route)
+
+        for route in radial_routes:
+            network.add_route(route)
+
+        for route in circular_routes:
+            network.add_route(route)
+
+        extended_graph = ExtendedGraph(graph_obj, network)
+
+        tree_graph = extended_graph.get_tree_graph()
+
+        one_stop = []
+        two_stop = []
+        three_stop = []
+
+        four_route_city = 0
+        six_route_city = 0
+        ten_route_city = 0
+
+        one_route_stop = 0
+        two_route_stop = 0
+        ten_route_stop = 0
+
+        for city_node in tree_graph:
+            if len(tree_graph[city_node]) == 1:
+                one_stop.append(city_node)
+            if len(tree_graph[city_node]) == 2:
+                two_stop.append(city_node)
+            if len(tree_graph[city_node]) == 3:
+                three_stop.append(city_node)
+
+            n_routes_city = 0
+            for stop in tree_graph[city_node]:
+                n_routes_stop = 0
+                for _ in tree_graph[city_node][stop]:
+                    n_routes_stop = n_routes_stop + 1
+                    n_routes_city = n_routes_city + 1
+
+                if n_routes_stop == 1:
+                    one_route_stop = one_route_stop + 1
+                if n_routes_stop == 2:
+                    two_route_stop = two_route_stop + 1
+                if n_routes_stop == 10:
+                    ten_route_stop = ten_route_stop + 1
+
+            if n_routes_city == 4:
+                four_route_city = four_route_city + 1
+            if n_routes_city == 6:
+                six_route_city = six_route_city + 1
+            if n_routes_city == 10:
+                ten_route_city = ten_route_city + 1
+
+        self.assertEqual(len(one_stop), 1)
+        self.assertEqual(len(two_stop), 5)
+        self.assertEqual(len(three_stop), 5)
+
+        self.assertEqual(four_route_city, 5)
+        self.assertEqual(six_route_city, 5)
+        self.assertEqual(ten_route_city, 1)
+
+        self.assertEqual(one_route_stop, 0)
+        self.assertEqual(two_route_stop, 25)
+        self.assertEqual(ten_route_stop, 1)
+
+
+
+
