9c0cccb54eda6935faed20f28677dbe792424c82
Felipe Vera <fvera@transapp.cl>
2020-07-05 15:36:35 -0400

    importamos routeType en init

--- sidermit/publictransportsystem/__init__.py
+++ sidermit/publictransportsystem/__init__.py
@@ -1,6 +1,6 @@
 from .mode import TransportMode, TransportModeManager
-from .network import TransportNetwork, Route
+from .network import TransportNetwork, Route, RouteType
 from .passenger import Passenger
 
 __all__ = ['TransportMode', 'TransportModeManager', 'TransportNetwork',
-           'Passenger', 'Route']
+           'Passenger', 'Route', 'RouteType']
