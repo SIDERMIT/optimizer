13db9f9478eb58fded62b9d4851bceba9004a872
Felipe Vera <fvera@transapp.cl>
2020-09-29 09:22:08 -0300

    correccion init city

--- sidermit/city/__init__.py
+++ sidermit/city/__init__.py
@@ -1,5 +1,5 @@
 from .demand import Demand
 from .graph import CBD, Periphery, Subcenter, Graph, GraphContentFormat, Node
 
-__all__ = ['CBD', 'Periphery', 'Subcenter', 'Graph', 'GraphFileFormat',
+__all__ = ['CBD', 'Periphery', 'Subcenter', 'Graph', 'GraphContentFormat',
            'Demand', 'Node']
