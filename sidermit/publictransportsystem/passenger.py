6b144473eb3adaefd614291afda1b09783c51f9a
Felipe Vera <fvera@transapp.cl>
2020-11-11 09:34:03 -0300

    actualización prict exceptions passengers

--- sidermit/publictransportsystem/passenger.py
+++ sidermit/publictransportsystem/passenger.py
@@ -25,21 +25,21 @@ class Passenger:
     va = passenger_property('_va', operator.le, VaIsNotValidExceptions(
         "You must give a value > 0 for va"), docstring="Walking speed [km/h]")
     pv = passenger_property('_pv', operator.lt, PvIsNotValidExceptions(
-        "You must give a value >= 0 for pv"), docstring="Value of in-vehicle time savings [US$/h]")
+        "You must give a value >= 0 for subjective pv"), docstring="Value of in-vehicle time savings [US$/h]")
     pw = passenger_property('_pw', operator.lt, PwIsNotValidExceptions(
-        "You must give a value >= 0 for pw"), docstring="Value of waiting time savings [US$/h]")
+        "You must give a value >= 0 for subjective  pw"), docstring="Value of waiting time savings [US$/h]")
     pa = passenger_property('_pa', operator.lt, PaIsNotValidExceptions(
-        "You must give a value >= 0 for pa"), docstring="Value of access time savings [US$/h]")
+        "You must give a value >= 0 for subjective  pa"), docstring="Value of access time savings [US$/h]")
     pt = passenger_property('_pt', operator.lt, PtIsNotValidExceptions(
-        "You must give a value >= 0 for pt"), docstring="Pure transfer penalty [EIV]")
+        "You must give a value >= 0 for subjective  pt"), docstring="Pure transfer penalty [EIV]")
     spv = passenger_property('_spv', operator.lt, SpvIsNotValidExceptions(
-        "You must give a value >= 0 for spv"), docstring="Social value of in-vehicle time savings [US$/h]")
+        "You must give a value >= 0 for social pv"), docstring="Social value of in-vehicle time savings [US$/h]")
     spw = passenger_property('_spw', operator.lt, SpwIsNotValidExceptions(
-        "You must give a value >= 0 for spw"), docstring="Social value of waiting time savings [US$/h]")
+        "You must give a value >= 0 for social pw"), docstring="Social value of waiting time savings [US$/h]")
     spa = passenger_property('_spa', operator.lt, SpaIsNotValidExceptions(
-        "You must give a value >= 0 for spa"), docstring="Social value of access time savings [US$/h]")
+        "You must give a value >= 0 for social  pa"), docstring="Social value of access time savings [US$/h]")
     spt = passenger_property('_spt', operator.lt, SptIsNotValidExceptions(
-        "You must give a value >= 0 for spt"), docstring="Social pure transfer penalty [EIV]")
+        "You must give a value >= 0 for social pt"), docstring="Social pure transfer penalty [EIV]")
 
     def __init__(self, va: float, pv: float, pw: float, pa: float, pt: float,
                  spv: float, spw: float, spa: float, spt: float):
